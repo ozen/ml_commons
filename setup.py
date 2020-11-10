65ec6b2e5c3ff688bcec686eee02517a40e9c05a
Yigit Ozen <canyigitozen@gmail.com>
2020-11-10 13:00:04 +0300

    Added BinaryFocalLoss

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ml_commons',
-    version='2.2.0',
+    version='2.3.0',
     author='Yigit Ozen',
     packages=find_packages(),
     install_requires=['numpy', 'tqdm'],
